2e15eddcbe91763de6136f038b44401d82b3432d
TE20203498 <TE20203498@APL-8LPFD63>
2021-06-25 19:00:32 +0530

    composite commit

---
+++ Composite-MS/src/main/java/com/composite/compositems/clients/CustomerServiceProxy.java
@@ -0,0 +1,21 @@
+package com.composite.compositems.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.composite.compositems.model.Customer;
+
+@FeignClient(name="Customer-Service")
+public interface CustomerServiceProxy {
+	
+	@PostMapping("/customer")
+	public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer);
+	
+	@GetMapping("/customer/{id}")
+	public Customer findCustomerById(@PathVariable("id") Long customerId);
+
+}
---
+++ Composite-MS/src/test/java/com/composite/compositems/CompositeMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.composite.compositems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompositeMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
