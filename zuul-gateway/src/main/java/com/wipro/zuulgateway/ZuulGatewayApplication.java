d793ea531d94bbc9570da31e6931b90290103fe5
hp <hp@LAPTOP-GC4K9BBM.mshome.net>
2021-06-26 12:46:19 +0530

    zuul and config server properties updated.

--- zuul-gateway/src/main/java/com/wipro/zuulgateway/ZuulGatewayApplication.java
+++ zuul-gateway/src/main/java/com/wipro/zuulgateway/ZuulGatewayApplication.java
@@ -1,27 +1,29 @@
 package com.wipro.zuulgateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-//import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
-//@EnableHystrix
+@RestController
 public class ZuulGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulGatewayApplication.class, args);
 	}
 
-//	@Value("${message}")
-//	String message;
-//
-//	@GetMapping("/")
-//	public String home() {
-//		return message;
-//	}
+	@Value("${message}")
+	String message;
+
+	@GetMapping("/")
+	public String home() {
+		return message;
+	}
 
 }
